2e7682007c49300d4258f5ea511ac89591cb661d
joseporras00 <55441394+joseporras00@users.noreply.github.com>
2020-11-13 00:03:26 +0100

    Update UserDAO.java

--- Pract2/src/bdd/UserDAO.java
+++ Pract2/src/bdd/UserDAO.java
@@ -28,18 +28,19 @@ public class UserDAO {
   // Método para insertar una fila
   // En ningún caso es recomendable el paso por parámetro de los valores individuales
   // Se recomienda utilizar el UserBean o una clase envoltorio User que tenga estas propiedades
-  public static int save(int id, String last, String first, int age){
+  public static int save(String nombre, String apellidos,String email,String fecha, int tags){
 	int status=0;
 	try{
 		Connection con=getConnection();
 		// PreparedStament será más rápido (si es uso recurrente) y permite invocación a parámetros
 		// Lo habitual es que las consultas y sentencias SQL estén en un fichero de propiedades aparte, no en código
-		PreparedStatement ps=con.prepareStatement("insert into User (id,last,first,age) values(?,?,?,?)");
+		PreparedStatement ps=con.prepareStatement("insert into User (nombre,apellidos,email,fecha,tags) values(?,?,?,?,?)");
 		// El orden de los parámetros debe coincidir con las '?' del código SQL
-		ps.setInt(1,id);
-		ps.setString(2,last);
-		ps.setString(3,first);
-		ps.setInt(4,age);
+		ps.setString(1,nombre);
+		ps.setString(2,apellidos);
+		ps.setString(3,email);
+		ps.setString(4,fecha);
+		ps.setInt(5,tags);
 		status = ps.executeUpdate();
 	// Importante capturar las excepciones. Si nuestra aplicaciones tiene más opciones de fallo,
 	// podemos capturar directamente SQLException
@@ -49,41 +50,44 @@ public class UserDAO {
 }
   
 // Método para actualizar un usuario
-public static int update(int id, String last, String first, int age){
+public static int update(String nombre, String apellidos,String email,String fecha, int tags){
 	int status=0;
 	try{
 		Connection con=getConnection();
-		PreparedStatement ps=con.prepareStatement("update User set last=?,first=?,age=? where id=?");
-		ps.setString(1,last);
-		ps.setString(2,first);
-		ps.setInt(3,age);
+		PreparedStatement ps=con.prepareStatement("update User set nombre=?,apellidos=?,fecha=?,tags=? where email=?");
+		ps.setString(1,nombre);
+		ps.setString(2,apellidos);
+		ps.setString(3,fecha);
+		ps.setInt(4,tags);
 		// En este caso, 'id' va después, conforme al orden de la sentencia SQL
-		ps.setInt(4,id);
+		ps.setString(5,email);
 		status=ps.executeUpdate();
 	}catch(Exception e){System.out.println(e);}
 	return status;
 }
 
 // Para la consulta, se ha tomado una estructura Hash (columna-tabla, valor)
-public static Hashtable<String,String> queryById (int id) {
+public static Hashtable<String,String> queryByEmail (String email) {
 	Statement stmt = null; 
 	Hashtable<String,String> resul = null;
 	try {
 		Connection con=getConnection();
 		// En consultas, se hace uso de un Statement 
 		stmt = con.createStatement();
-	    ResultSet rs = stmt.executeQuery("select last, first, age from User where id = " + id);
+	    ResultSet rs = stmt.executeQuery("select nombre, apellidos, fecha, tags from User where email = " + email);
 	    while (rs.next()) {
-	    	String last = rs.getString("last");
-	        String first = rs.getString("first");
-	        int age = rs.getInt("age");
+	    	String nombre = rs.getString("nombre");
+	        String apellidos = rs.getString("apellidos");
+		String fecha = rs.getString("fecha");
+	        int tags = rs.getInt("tags");
 	        resul = new Hashtable<String,String>();
-	        resul.put("id", Integer.toString(id));
-	        resul.put("last", last);
-	        resul.put("first", first);
-	        resul.put("age", Integer.toString(age));        
-	        System.out.println(id + "\t" + last +
-	                               "\t" + first + "\t" + age);
+	        resul.put("Nombre", nombre);
+	        resul.put("Apellidos", apellidos);
+		resul.put("Email", email);
+	        resul.put("F. Nacimiento", fecha);
+	        resul.put("tags", Integer.toString(tags));        
+	        System.out.println(nombre + "\t" + apellidos +
+	                               "\t" + email + "\t" + fecha + "\t" + tags);
 	    }
 	    // Se debe tener precaución con cerrar las conexiones, uso de auto-commit, etc.
 	    if (stmt != null) 
@@ -94,12 +98,12 @@ public static Hashtable<String,String> queryById (int id) {
 	return resul;
 } 
 
-public static int delete(int id){
+public static int delete(String email){
 	int status=0;
 	try{
 		Connection con=getConnection();
-		PreparedStatement ps=con.prepareStatement("delete from User where id=?");
-		ps.setInt(1,id);
+		PreparedStatement ps=con.prepareStatement("delete from Usuarios where email=?");
+		ps.setString(1,email);
 		status=ps.executeUpdate();
 	}catch(Exception e){System.out.println(e);}
 
