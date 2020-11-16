71051e3abc17ffaef087a1b5560b7a2df405a398
joseporras00 <55441394+joseporras00@users.noreply.github.com>
2020-11-16 19:54:28 +0100

    Update AnuncioDAO.java

--- Pract2/src/bdd/AnuncioDAO.java
+++ Pract2/src/bdd/AnuncioDAO.java
@@ -17,7 +17,7 @@ public class AnuncioDAO {
 	  Class.forName("com.mysql.jdbc.Driver");
 	  // Introducir correctamente el nombre y datos de conexión - Idealmente, estos datos se 
 	  // indican en un fichero de propiedades
-	  con= DriverManager.getConnection("jdbc:mysql://oraclepr.uco.es:3306/basedatos","usuario","password");
+	  con= DriverManager.getConnection("jdbc:mysql://oraclepr.uco.es:3306/basedatos",conf.usuario,conf.pass);
 	// Importante capturar 
 	} catch(Exception e) {
 	  System.out.println(e);
@@ -28,23 +28,23 @@ public class AnuncioDAO {
   // Método para insertar una fila
   // En ningún caso es recomendable el paso por parámetro de los valores individuales
   // Se recomienda utilizar el UserBean o una clase envoltorio User que tenga estas propiedades
-  public static int save(String nombre, String apellidos,String email,String fecha, int tags){
+  public static int save(int id,String titulo, String propietario, String destinatario, String cuerpo, String fase, String tag, String fcom, String ffin){
 	int status=0;
 	try{
 		Connection con=getConnection();
 		// PreparedStament será más rápido (si es uso recurrente) y permite invocación a parámetros
 		// Lo habitual es que las consultas y sentencias SQL estén en un fichero de propiedades aparte, no en código
-		PreparedStatement ps=con.prepareStatement("insert into Anuncios (id,titulo,propietario,destinatarios,cuerpo,fase,tag,fcom,ffin) values(?,?,?,?,?,?,?,?,?)");
+		PreparedStatement ps=con.prepareStatement(sql.Asave);
 		// El orden de los parámetros debe coincidir con las '?' del código SQL
 		ps.setInt(1,id);
 		ps.setString(2,titulo);
 		ps.setString(3,propietario);
 		ps.setString(4,destinatarios);
 		ps.setString(5,cuerpo);
-    ps.setString(6,fase);
-    ps.setString(7,tag);
-    ps.setString(8,fcom);
-    ps.setString(9,ffin);
+    		ps.setString(6,fase);
+    		ps.setString(7,tag);
+    		ps.setString(8,fcom);
+    		ps.setString(9,ffin);
 		status = ps.executeUpdate();
 	// Importante capturar las excepciones. Si nuestra aplicaciones tiene más opciones de fallo,
 	// podemos capturar directamente SQLException
@@ -54,19 +54,19 @@ public class AnuncioDAO {
 }
   
 // Método para actualizar un usuario
-public static int update(String nombre, String apellidos,String email,String fecha, int tags){
+public static int update(String titulo, String propietario, String destinatario, String cuerpo, String fase, String tag, String fcom, String ffin, int id){
 	int status=0;
 	try{
 		Connection con=getConnection();
-		PreparedStatement ps=con.prepareStatement("update Anuncios set titulo=?,propietario=?,destinatarios=?,cuerpo=?,fase=?,tag=?,fcom=?,ffin=? where id=?");
+		PreparedStatement ps=con.prepareStatement(sql.Aupdate);
 		ps.setString(1,titulo);
 		ps.setString(2,propietario);
 		ps.setString(3,destinatarios);
-    ps.setString(4,cuerpo);
-    ps.setString(5,fase);
-    ps.setString(6,tag);
-    ps.setString(7,fcom);
-    ps.setString(8,ffin);
+    		ps.setString(4,cuerpo);
+    		ps.setString(5,fase);
+    		ps.setString(6,tag);
+    		ps.setString(7,fcom);
+    		ps.setString(8,ffin);
 		ps.setInt(9,id);
 		// En este caso, 'id' va después, conforme al orden de la sentencia SQL
 		
@@ -83,27 +83,27 @@ public static Hashtable<String,String> queryById (int id) {
 		Connection con=getConnection();
 		// En consultas, se hace uso de un Statement 
 		stmt = con.createStatement();
-	    ResultSet rs = stmt.executeQuery("select titulo, propietario, destinatarios, cuerpo, fase, tag, fcom, ffin from Anuncios where id = " + id);
+	    ResultSet rs = stmt.executeQuery(sql.Aselect);
 	    while (rs.next()) {
-	    	  String titulo = rs.getString("titulo");
+	    	String titulo = rs.getString("titulo");
 	        String propietario = rs.getString("propietario");
-		      String destinatarios = rs.getString("destinatarios");
-          String cuerpo = rs.getString("cuerpo");
-          String fase = rs.getString("fase");
-          String tag = rs.getString("tag");
-          String fcom = rs.getString("fcom");
-          String ffin = rs.getString("ffin");
+		String destinatarios = rs.getString("destinatarios");
+         	String cuerpo = rs.getString("cuerpo");
+         	String fase = rs.getString("fase");
+         	String tag = rs.getString("tag");
+         	String fcom = rs.getString("fcom");
+         	String ffin = rs.getString("ffin");
 	        int id = rs.getInt("id");
 	        resul = new Hashtable<String,String>();
 	        resul.put("Id", Integer.toString(id));
 	        resul.put("Titulo", titulo);
-		      resul.put("Propietario", propietario);
+		resul.put("Propietario", propietario);
 	        resul.put("Destinatarios", destinatarios);
-          resul.put("Cuerpo", cuerpo);
-          resul.put("Fase", fase);
-          resul.put("Tag", tag);
-          resul.put("F. Comienzo", fcom);
-          resul.put("F. Fin", ffin);    
+          	resul.put("Cuerpo", cuerpo);
+          	resul.put("Fase", fase);
+          	resul.put("Tag", tag);
+          	resul.put("F. Comienzo", fcom);
+          	resul.put("F. Fin", ffin);    
 	        System.out.println(id + "\n" + titulo +
 	                               "\n" + propietario + "\n" + destinatarios + "\n" + cuerpo + "\n" + fase + "\n" + tag + "\n" + fcom + "\n" + ffin + "\n\n");
 	    }
@@ -120,7 +120,7 @@ public static int delete(int id){
 	int status=0;
 	try{
 		Connection con=getConnection();
-		PreparedStatement ps=con.prepareStatement("delete from Anuncios where id=?");
+		PreparedStatement ps=con.prepareStatement(sql.Adelete);
 		ps.setId(1,id);
 		status=ps.executeUpdate();
 	}catch(Exception e){System.out.println(e);}
